4c2ddec6fb41785d4e82bc2c13ca71c449517b54
thecitystate <thecitystate@thecitystate-desktop.(none)>
2010-05-24 20:46:05 -0400

    included copy of Python 2.6 with valgrind supressions

---
+++ Python-2.6.5/build/temp.linux-i686-2.6/libffi/include/ffi_common.h
@@ -0,0 +1,1 @@
+/home/thecitystate/shooter/Python-2.6.5/Modules/_ctypes/libffi/include/ffi_common.h
---
+++ Python-2.6.5/build/temp.linux-i686-2.6/libffi/include/ffitarget.h
@@ -0,0 +1,1 @@
+/home/thecitystate/shooter/Python-2.6.5/Modules/_ctypes/libffi/src/x86/ffitarget.h
