c76ff8d5a90005668da169ce8e174f2de80d1a89
Tyler Dodge <tldodge@ncsu.edu>
2011-12-07 21:06:50 -0500

    Removed local Python

--- Python-2.6.5/build/temp.linux-i686-2.6/libffi/include/ffi_common.h
+++
@@ -1,1 +0,0 @@
-/home/thecitystate/shooter/Python-2.6.5/Modules/_ctypes/libffi/include/ffi_common.h
--- Python-2.6.5/build/temp.linux-i686-2.6/libffi/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/home/thecitystate/shooter/Python-2.6.5/Modules/_ctypes/libffi/src/x86/ffitarget.h
